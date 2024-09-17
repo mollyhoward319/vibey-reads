f61c201156578a7b8b726c5ea29da99640edf260
RonahNkatha <124624805+RonahNkatha@users.noreply.github.com>
2024-09-17 18:09:19 -0400

    Added user model and vibes model

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,8 @@
-import express from 'express';
-import sequelize from '';
+// CommonJS and Modules
+import express from "express";
+import sequelize from "";
+import dotenv from "dotenv";
+dotenv.config();
 // import routes from '';
 
 const app = express();
@@ -12,9 +15,9 @@ app.use(routes);
 // Force true to drop/recreate table(s) on every sync
 // when we deploay turn force:false. to drop and create new tables we want to switch it to true to fix the bugs in our tables
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is listening on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
 });
 
 // activity 14.03.
