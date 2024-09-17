66bd80ce2dd650abef8529760cf28310cff1a1b2
Molly Howard <mollyhoward319@gmail.com>
2024-09-16 20:41:55 -0500

    delete music table

---
+++ server/src/models/music-delete.js
@@ -0,0 +1,294 @@
+album_id: {
+    type: dataTypes.STRING,
+},
+album_name: {
+    type: dataTypes.STRING,
+},
+album_total_tracks: {
+    type: dataTypes.INTEGER,
+},
+album_artists_external_urls: {
+    type: dataTypes.OBJECT,
+},
+album_artists_href: {
+    type: dataTypes.STRING,
+},
+album_artists_id: {
+    type: dataTypes.STRING,
+},
+album_artists_type: {
+    type: dataTypes.STRING,
+},
+album_tracks_href: {
+    type: dataTypes.STRING,
+},
+album_tracks_external_ids_upc: {
+    type: dataTypes.STRING,
+},
+album_genres: {
+    type: dataTypes.ARRAY,
+},
+album_release_date: {
+    type: dataTypes.STRING,
+},
+album_popularity: {
+    type: dataTypes.INTEGER,
+},
+album_images: {
+    type: dataTypes.ARRAY,
+},
+album_href: {
+    type: dataTypes.STRING,
+},
+track_id: {
+    type: dataTypes.STRING,
+},
+track_album: {
+    type: dataTypes.STRING,
+},
+track_total_tracks: {
+    type: dataTypes.INTEGER,
+},
+track_external_urls: {
+    type: dataTypes.OBJECT,
+},
+track_href: {
+    type: dataTypes.STRING,
+},
+track_images_url: {
+    type: dataTypes.STRING,
+},
+track_images_height: {
+    type: dataTypes.INTEGER,
+},
+track_images_width: {
+    type: dataTypes.INTEGER,
+},
+track_name: {
+    type: dataTypes.STRING,
+},
+track_release_date: {
+    type: dataTypes.STRING,
+},
+track_artist_external_urls: {
+    type: dataTypes.OBJECT,
+},
+track_href: {
+    type: dataTypes.STRING,
+},
+track_id: {
+    type: dataTypes.STRING,
+},
+track_name: {
+    type: dataTypes.STRING,
+},
+track_type: {
+    type: dataTypes.STRING,
+},
+track_artists_external_urls: {
+    type: dataTypes.OBJECT,
+},
+track_artists_href: {
+    type: dataTypes.STRING,
+},
+track_artists_name: {
+    type: dataTypes.STRING,
+},
+track_artists_id: {
+    type: dataTypes.STRING,
+},
+track_artists_type: {
+    type: dataTypes.STRING,
+},
+track_duration_ms: {
+    type: dataTypes.INTEGER,
+},
+track_explicit: {
+    type: dataTypes.BOOLEAN,
+},
+track_external_ids_upc: {
+    type: dataTypes.STRING,
+},
+track_href: {
+    type: dataTypes.STRING,
+},
+track_id: {
+    type: dataTypes.STRING,
+},
+track_name: {
+    type: dataTypes.STRING,
+},
+track_images_url: {
+    type: dataTypes.STRING,
+},
+track_popularity: {
+    type: dataTypes.INTEGER,
+},
+track_preview_url: {
+    type: dataTypes.STRING,
+},
+track_track_number: {
+    type: dataTypes.INTEGER,
+},
+track_type: {
+    type: dataTypes.STRING,
+},
+artist_id: {
+    type: dataTypes.STRING,
+},
+artist_name: {
+    type: dataTypes.STRING,
+},
+artist_external_urls: {
+    type: dataTypes.STRING,
+},
+artist_genres: {
+    type: dataTypes.ARRAY,
+},
+artist_href: {
+    type: dataTypes.STRING,
+},
+artist_images_url: {
+    type: dataTypes.STRING,
+},
+artist_images_height: {
+    type: dataTypes.INTEGER,
+},
+artist_images_width: {
+    type: dataTypes.INTEGER,
+},
+artist_popularity: {
+    type: dataTypes.INTEGER,
+},
+track_id: {
+    type: dataTypes.STRING,
+},
+track_album: {
+    type: dataTypes.STRING,
+},
+track_total_tracks: {
+    type: dataTypes.INTEGER,
+},
+track_external_urls: {
+    type: dataTypes.OBJECT,
+},
+track_href: {
+    type: dataTypes.STRING,
+},
+track_images_url: {
+    type: dataTypes.STRING,
+},
+track_images_height: {
+    type: dataTypes.INTEGER,
+},
+track_images_width: {
+    type: dataTypes.INTEGER,
+},
+track_name: {
+    type: dataTypes.STRING,
+},
+track_release_date: {
+    type: dataTypes.STRING,
+},
+track_artist_external_urls: {
+    type: dataTypes.OBJECT,
+},
+track_href: {
+    type: dataTypes.STRING,
+},
+track_id: {
+    type: dataTypes.STRING,
+},
+track_name: {
+    type: dataTypes.STRING,
+},
+track_type: {
+    type: dataTypes.STRING,
+},
+track_artists_external_urls: {
+    type: dataTypes.OBJECT,
+},
+track_artists_href: {
+    type: dataTypes.STRING,
+},
+track_artists_name: {
+    type: dataTypes.STRING,
+},
+track_artists_id: {
+    type: dataTypes.STRING,
+},
+track_artists_type: {
+    type: dataTypes.STRING,
+},
+track_duration_ms: {
+    type: dataTypes.INTEGER,
+},
+track_explicit: {
+    type: dataTypes.BOOLEAN,
+},
+track_external_ids_upc: {
+    type: dataTypes.STRING,
+},
+track_href: {
+    type: dataTypes.STRING,
+},
+track_id: {
+    type: dataTypes.STRING,
+},
+track_name: {
+    type: dataTypes.STRING,
+},
+track_images_url: {
+    type: dataTypes.STRING,
+},
+track_popularity: {
+    type: dataTypes.INTEGER,
+},
+track_preview_url: {
+    type: dataTypes.STRING,
+},
+track_track_number: {
+    type: dataTypes.INTEGER,
+},
+track_type: {
+    type: dataTypes.STRING,
+},
+search_q: {
+    type: dataTypes.STRING,
+},
+search_tracks_href: {
+    type: dataTypes.STRING,
+},
+search_tracks_next: {
+    type: dataTypes.STRING,
+},
+search_tracks_previous: {
+    type: dataTypes.STRING,
+},
+search_artists_href: {
+    type: dataTypes.STRING,
+},
+search_artists_next: {
+    type: dataTypes.STRING,
+},
+search_artists_previous: {
+    type: dataTypes.STRING,
+},
+search_artists_items: {
+    type: dataTypes.ARRAY,
+},
+search_albums_href: {
+    type: dataTypes.STRING,
+},
+search_albums_next: {
+    type: dataTypes.STRING,
+},
+search_albums_previous: {
+    type: dataTypes.STRING,
+},
+search_albums_items: {
+    type: dataTypes.STRING,
+},
+search_albums_external_urls: {
+    type: dataTypes.OBJECT,
+},
