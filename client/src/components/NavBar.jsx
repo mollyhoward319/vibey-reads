75aa122c11844ea89a836be5eb917a126e6b6421
RonahNkatha <124624805+RonahNkatha@users.noreply.github.com>
2024-09-19 20:29:09 -0400

    Added routes navigation, login and signup forms

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,2 +1,24 @@
 // todo: create nav bar that is static and in every part of the app except login
 // will have: User Name or Username as the brand, a button for Whats Your Vibe? Maybe logo too
+
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,3 +1,24 @@
 // todo: login logic
 // todo: sign-up logic
 // what needs to populate here: logo, 1 form for login with a login button, the login button will take you to th homepage, a sign-up botton will start the process of a sign-up form either in popup or another page. on finish sign-up they will go back to log-in
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="email">Email: </label>
+          <input type="email" id="email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password: </label>
+          <input type="password" id="password" />
+        </div>
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
