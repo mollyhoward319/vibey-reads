ae09248ceb0d5ef1cc4b2727b2626b146f3a7c45
“[firstname <dafne.faviola.luna@gmail.com>
2024-09-21 18:16:41 -0400

    updated index.js in api folder

--- server/src/routes/api/index.js
+++ server/src/routes/api/index.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { userRouter } from "./user-routes.js";
+import { bookRouter } from "./booksroutes.js";
+import { musicRouter } from "./spotifyRoutes.js";
 
 const router = Router();
 
-// Prefix all routes defined in `bookRoutes.js` with `/books
-// router.use('/books', bookRouter);
 router.use("/users", userRouter);
+router.use("/books", bookRouter); // Prefix all routes defined in `bookRoutes.js` with `/books
+router.use("/music", musicRouter); // Prefix all routes defined in "spotifyRoutes.js" with /music
 
 export default router;
 
-// activity 14.05
