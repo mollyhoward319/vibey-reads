ed6ab82969cf5460678df225bf453500f59a9559
[mollyhoward] <mollyhoward319@gmail.com>
2024-09-22 18:15:26 -0500

    edited titles

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,10 +4,10 @@ import './Header.css'; // Import CSS for styling
 const Header = () => {
   return (
     <header className="header">
-      <h1>My App</h1>
+      <h1>Vibey Reads</h1>
       <nav>
         <ul>
-          <li><a href="/">Home</a></li>
+          <li><a href="/">What's your Vibe?</a></li>
           <li><a href="/about">About</a></li>
           <li><a href="/contact">Contact</a></li>
         </ul>
