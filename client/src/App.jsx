f34f7ad0319ea6e5960f986519664714ebe42c0d
[mollyhoward] <mollyhoward319@gmail.com>
2024-09-21 23:03:38 -0500

    edits to app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import App1 from "./pages/Profile.jsx";
 
+function App() {
+  return (
+    <div>
+      <App1 />     
+    </div>
+  );
+}
+
+export default App;
 
 
 // const SPOTIFY_CLIENT_ID = "";
