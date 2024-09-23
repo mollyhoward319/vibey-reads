8385d27aad94485e6328c5e30d7fcd6353af1fdb
RonahNkatha <124624805+RonahNkatha@users.noreply.github.com>
2024-09-23 19:28:19 -0400

    modified signup and login

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,32 +1,31 @@
-import dotenv from "dotenv";
-dotenv.config();
 import sequelize from "../config/connection.js";
-import { Vibes } from "./vibes.js";
-import { Music } from "./music.js";
-import { Bookshelf } from "./bookshelf.js";
-import { UserFactory } from "./user.js";
+import VibesFactory from "./vibes.js";
+import MusicFactory from "./music.js";
+import BookFactory from "./bookshelf.js";
+import UserFactory from "./user.js";
 
-const User = UserFactory(sequelize);
-const Books = Bookshelf(sequelize);
-const Playlist = Music(sequelize);
-// const Vibe = Vibes(sequelize);
+const Viber = UserFactory(sequelize);
+console.log("-------", Viber);
+// const Viber = new User;
+const Books = BookFactory(sequelize);
+const Playlist = MusicFactory(sequelize);
+const Vibe = VibesFactory(sequelize);
 
 // What/How are each table realated/associated?
 // User can have many Vibes
-// User.hasMany(Vibe, {foreignKey: "user_id",});
-// Vibe.belongsTo(User, {foreignKey: "user_id",});
+Viber.hasMany(Vibe, { foreignKey: "user_id" });
+Vibe.belongsTo(Viber, { foreignKey: "user_id" });
 
-// // A Vibe HAS A Book, has a playlist, and a user
-// Vibe.hasOne(Books, {foreignKey: "book_id",});
-// Vibe.hasOne(Playlist, {foreignKey: "music_id"});
-// // Vibe.hasOne(User, {foreignKey: "id"}); may not need
+// A Vibe HAS A Book, has a playlist, and a user
+Vibe.hasOne(Books, { foreignKey: "book_id" });
+Vibe.hasOne(Playlist, { foreignKey: "music_id" });
+// Vibe.hasOne(User, {foreignKey: "id"}); may not need
 
-//   // Books belongs to many vibes
-// Books.belongsTo(Vibe, {foreignKey: "book_id",});
-// // Music belongs to vibes
-// Playlist.belongsTo(Vibe, {foreignKey: "music_id",});
-// // User belongs to vibe
-// User.hasOne(Vibe, { foreignKey: "vibe_id" });
+// Books belongs to many vibes
+Books.belongsTo(Vibe, { foreignKey: "book_id" });
+// Music belongs to vibes
+Playlist.belongsTo(Vibe, { foreignKey: "music_id" });
+// User belongs to vibe
+Viber.hasOne(Vibe, { foreignKey: "vibe_id" });
 
-// export { sequelize, User , Books, Playlist, Vibe};
-export { sequelize, User };
+export { Viber, Books, Playlist, Vibe };
