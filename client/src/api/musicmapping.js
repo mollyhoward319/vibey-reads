49a26ebc892422ff82d0c222896271a9c2b5712e
“[firstname <dafne.faviola.luna@gmail.com>
2024-09-23 10:18:48 -0400

    pushing back both Sabrina and my prs

--- client/src/api/musicmapping.js
+++
@@ -1,8 +0,0 @@
-// export const getAllmusic = async() => {
-//     const response = await fetch("/api/Playlist");
-//     const data = await response.json();
-//     console.log("______", data);
-//     return data;
-// }
-
-// will delete. combigning into 1 api call file
