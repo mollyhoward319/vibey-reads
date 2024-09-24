bfdf46a9db619f8a62bcc6a66f7b5fb7070ad7e2
[mollyhoward] <mollyhoward319@gmail.com>
2024-09-23 23:14:32 -0500

    edited header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import './Header.css'; // Import CSS for styling
+
 
 const Header = () => {
   return (
     <header className="header">
-      <h1>Vibey Reads</h1>
+      <h1>Let's get vibin'</h1>
       <nav>
         <ul>
-        <li><a href="/">What's your Vibe?</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
+        {/* <li>Welcome, {username}!</li> Display the username */}
         </ul>
       </nav>
     </header>
