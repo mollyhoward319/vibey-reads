915af9b9b5b8a3070dde3c1c3eb9f14e18808c2c
[mollyhoward] <mollyhoward319@gmail.com>
2024-09-23 23:16:13 -0500

    added logo to login page

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,10 +1,13 @@
 // todo: login logic
 // todo: sign-up logic
 // what needs to populate here: logo, 1 form for login with a login button, the login button will take you to th homepage, a sign-up botton will start the process of a sign-up form either in popup or another page. on finish sign-up they will go back to log-in
+import logo from '../assets/VibeyReadsLong.png'
 
 const Login = () => {
   return (
     <div className="form-wrapper">
+       <img src={logo} alt="Vibey Reads Logo" className="logo" style={{ width: '750px', height: 'auto', marginBottom: '50px' }}/>
+
       <h2>Login</h2>
       <form>
         <div className="form-group">
