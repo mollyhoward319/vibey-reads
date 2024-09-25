b163654055608b84f244cf01cbdde98d319f319c
“[firstname <dafne.faviola.luna@gmail.com>
2024-09-24 22:49:44 -0400

    fixed render allegedly

--- server/src/server.js
+++ server/src/server.js
@@ -8,11 +8,10 @@ import routes from "./routes/index.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+app.use(express.static("client/dist"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static("../client/dist"));
 app.use(routes);
 
 // Force true to drop/recreate table(s) on every sync
